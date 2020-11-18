3753d3a4cf0e9f510cdfcb09a873e4ef33551fac
Dingus94 <thomas.letourneau@hotmail.com>
2020-11-18 01:15:07 -0500

    Adding how to add text to an image.

--- Img_drawing.py
+++ Img_drawing.py
@@ -1,15 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
 
-image = Image.open(r"IMAGE")
+image = Image.open(r"C:\Users\Thoma\OneDrive\Pictures\hornyjail.png")
 
-font_type = ImageFont.truetype('arial.ttf', 30)
-font_type_2 = ImageFont.truetype('arialbd.ttf', 30)
-font_type_3 = ImageFont.truetype('ariali.ttf', 30)
+font_type = ImageFont.truetype('arialbd.ttf', 60)
 
 draw = ImageDraw.Draw(image)
 
-draw.text(xy = (50, 50), text = "Text One", fill = (255,69,0), font = font_type)
-draw.text(xy = (0, 0), text = "Text Two", fill = (255,69,0), font = font_type_2)
-draw.text(xy = (100, 100), text = "Text Three", fill = (255,69,0), font = font_type_3)
+draw.text(xy = (975, 650), text = "My Cool Bot", fill = (0,0,0), font = font_type)
 
 image.show()
--- cogs/utils.py
+++ cogs/utils.py
@@ -25,34 +25,8 @@ class Utils(commands.Cog):
 
         await ctx.send(':wave:', embed = embed)
 
-    @commands.command()
-    async def hornyjail(self, ctx, user: discord.user = None):
-        if user is None:
-            await ctx.send('Please provide the username of the horny perpetrator!')
-            return
-
-        from PIL import Image, ImageDraw, ImageFont
-        # get an image
-        base = Image.open("Pillow/Tests/images/hopper.png").convert("RGBA")
-
-        # make a blank image for the text, initialized to transparent text color
-        txt = Image.new("RGBA", base.size, (255,255,255,0))
-
-        # get a font
-        fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
-        # get a drawing context
-        d = ImageDraw.Draw(txt)
-
-        # draw text, half opacity
-        d.text((10,10), "Hello", font=fnt, fill=(255,255,255,128))
-        # draw text, full opacity
-        d.text((10,60), "World", font=fnt, fill=(255,255,255,255))
-
-        out = Image.alpha_composite(base, txt)
 
         out.show()
-
-
 
 
 def setup(client):
